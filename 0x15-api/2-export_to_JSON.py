b8edeba08289d62c77a51efec9633e35cb3a574b
bradley-mwangangi <bradley-mwangangi@github.com>
2024-08-01 03:39:54 +0300

    add 2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import json
+import requests
+import sys
+"""
+    exports data in the JSON format
+"""
+
+
+def get_employee_todo_progress(emp_id):
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={emp_id}"
+
+    employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
+
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    username = employee_data.get("username")
+
+    tasks_list = []
+    for task in todos_data:
+        tasks_list.append({
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": username
+        })
+
+    json_data = {str(emp_id): tasks_list}
+
+    json_filename = f"{emp_id}.json"
+    with open(json_filename, mode='w') as json_file:
+        json.dump(json_data, json_file)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+    else:
+        try:
+            emp_id = int(sys.argv[1])
+            get_employee_todo_progress(emp_id)
+        except ValueError:
+            print("Please provide a valid integer as employee_id")
